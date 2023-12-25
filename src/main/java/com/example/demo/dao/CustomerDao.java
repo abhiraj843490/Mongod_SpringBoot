4bcda13070683485f1af2cd024f0230bbcde4466
Abhiraj kumar <abhiraj20abc@gmail.com>
2023-12-25 18:35:53 +0530

    revert

--- src/main/java/com/example/demo/dao/CustomerDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.dao;
-
-import com.example.demo.entity.Customer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CustomerDao extends MongoRepository<Customer, Integer> {
-}
--- src/main/java/com/example/demo/exception/CustomerAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exception;
-
-public class CustomerAlreadyExistsException extends RuntimeException {
-    public CustomerAlreadyExistsException(String message) {
-        super(message);
-    }
-}
